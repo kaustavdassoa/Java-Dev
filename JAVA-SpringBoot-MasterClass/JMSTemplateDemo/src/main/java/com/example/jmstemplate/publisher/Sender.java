6ecd627db42420119f9b86d694d88fa9541450b8
kaustavDellLap <kaustavdassoa@gmail.com>
2020-12-24 20:29:49 +0530

    Updating Sender Code

--- JAVA-SpringBoot-MasterClass/JMSTemplateDemo/src/main/java/com/example/jmstemplate/publisher/Sender.java
+++ JAVA-SpringBoot-MasterClass/JMSTemplateDemo/src/main/java/com/example/jmstemplate/publisher/Sender.java
@@ -1,23 +1,14 @@
 package com.example.jmstemplate.publisher;
 
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 import org.springframework.stereotype.Component;
 
-import com.example.jmstemplate.exception.SampleJmsErrorHandler;
 
 @Component
 public class Sender {
@@ -32,6 +23,8 @@ public class Sender {
               return session.createTextMessage(message);
             }
 			});
+			
+			
 		}
 	
 	
