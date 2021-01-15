54833b7ab590d754dbab9691f202bfed809d5531
kaustavDellLap <kaustavdassoa@gmail.com>
2021-01-15 18:14:12 +0530

    intial commit

---
+++ JAVA-SpringBoot-MasterClass/HazelcastDemo/src/main/java/com/example/hazelcastdemo/HazelcastDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.hazelcastdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@@ REF LINK - https://medium.com/@ihorkosandiak/spring-boot-with-hazelcast-b04d13927745
+
+@SpringBootApplication
+public class HazelcastDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastDemoApplication.class, args);
+	}
+
+}
