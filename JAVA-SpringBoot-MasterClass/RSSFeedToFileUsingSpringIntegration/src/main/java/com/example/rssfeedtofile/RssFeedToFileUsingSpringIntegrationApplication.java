84decdb30720bbec87040e8abf57fbf228c9236c
kaustavDellLap <kaustavdassoa@gmail.com>
2021-03-23 20:19:00 +0530

    intail commit

---
+++ JAVA-SpringBoot-MasterClass/RSSFeedToFileUsingSpringIntegration/src/main/java/com/example/rssfeedtofile/RssFeedToFileUsingSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.rssfeedtofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedToFileUsingSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedToFileUsingSpringIntegrationApplication.class, args);
+	}
+
+}
